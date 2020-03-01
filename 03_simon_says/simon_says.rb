50c2a6f0c694612171930df4d763467003b5ee1a
Zaid Al-ruwaishan <zaidruwaishan@gmail.com>
2020-03-01 18:43:56 -0500

    Simon says program with passing tests

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
 #write your code here
+def echo(word)
+    word
+end
+
+def shout(word)
+    word.upcase
+end
+
+def repeat(word, count = 2)
+    repeated = ""
+    count.times { repeated += word + " " }
+    repeated.delete_suffix(" ")
+end
+
+def start_of_word(word, length = 1)
+    word[0..length - 1]
+end
+
+def first_word(word)
+    word[0..word.index(" ") - 1]
+end
+
+def titleize(word)
+    words = word.split
+    words[0].capitalize!
+    words.each do |word|
+        if word.length > 3
+            word.capitalize!
+        end
+    end
+    words.join(" ")
+end
