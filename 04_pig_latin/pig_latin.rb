1592d1ed4eb890b123b3a572929be6696f2c69de
Zaid Al-ruwaishan <zaidruwaishan@gmail.com>
2020-03-01 19:08:25 -0500

    Pig latin program with passing tests

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,25 @@
 #write your code here
+vowels = ["a", "e", "i", "o", "u"]
+
+def translate(words)
+    words_arr = words.split();
+    pig_latin_word = ""
+    words_arr.each {
+        |x| pig_latin_word += to_pig_latin(x) + " "
+    }
+    pig_latin_word = pig_latin_word[0..-2]
+    return pig_latin_word
+end
+
+def to_pig_latin(word)
+    while !is_vowel(word[0])
+        word += word[0]
+        word = word[1..-1]
+    end 
+    word += "ay"
+    return word
+end
+
+def is_vowel(letter)
+    return ["a", "e", "i", "o"].include? letter
+end
